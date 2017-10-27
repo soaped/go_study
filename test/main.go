e894c5451cf1bc1c534b9d9c57596ba9b380ed1c
杨福照 <yangfuzhao@ipaynow.cn>
2017-10-27 17:41:02 +0800

    1

--- test/main.go
+++ test/main.go
@@ -6,5 +6,18 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	var grade string = "B"
+	var marks = 91
+
+	switch marks {
+	case 90:
+		grade = "A"
+	default:
+		grade = "D"
+	}
+
+	switch  {
+	case grade == "A": print("成绩不错")
+	case grade == "D": print("没及格")
+	}
 }
