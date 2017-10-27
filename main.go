bfa0a799bb688fa8a075835e6a1e2afb79f88e31
杨福照 <yangfuzhao@ipaynow.cn>
2017-10-27 17:37:24 +0800

    q

---
+++ main.go
@@ -0,0 +1,21 @@
+// test project main.go
+package main
+
+func main() {
+
+	var grade string = "B"
+	var marks = 91
+
+	switch marks {
+	case 90:
+		grade = "A"
+	default:
+		grade = "D"
+	}
+
+	switch  {
+	case grade == "A": print("成绩不错")
+	case grade == "D": print("没及格")
+	}
+
+}
